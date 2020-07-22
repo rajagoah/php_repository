2e02c156ae930bb7a9b3b4e2c6bc93e57c1aa38b
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-07-21 20:44:01 -0400

    code commit

---
+++ variables_in_php.php
@@ -0,0 +1,10 @@
+<?php
+    $fname = "Aakarsh";
+    $lname = "Rajagopalan";
+
+    echo "$fname $lname \n";
+    
+    //using the echo command to display the messages within html tag
+    echo "\nusing the echo command to display the messages within html tag";
+    echo "<h2>$fname $lname</h2>";
+?>
