953a56122b4fb5ae160eba41ad5ecaa58566179c
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-07-23 14:02:00 -0400

    code commit

---
+++ foreach.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+
+$nums = [1,2,3,4,34,33] ;
+    foreach($nums as $n){
+        echo $n."<br>";
+    }
+    echo "loop complete";
+?>
+
+
+
+</body>
+</html>
