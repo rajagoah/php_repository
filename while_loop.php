1ef5b3dd4f44110fd1f8e802b008c47f4f1a3f45
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-07-23 13:53:13 -0400

    code commit

---
+++ while_loop.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>While Loops</title>
+</head>
+<body>
+
+<?php 
+
+$cntr = 0;
+    while($cntr < 8){
+        echo "counter value is: ".$cntr."<br>";
+        $cntr += 1;
+    }
+
+    echo "loop complete";
+?>
+
+
+
+</body>
+</html>
