b3c7949c246d90bc50ac95744e5ebc9073c37bb6
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-07-23 13:57:40 -0400

    code commit

---
+++ for_loop.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+
+//using for loop
+    for ($i = 0; $i<10; $i++){
+        echo "The value of 'i' is: ".$i."<br>";
+    }
+    echo "loop complete"
+    
+    
+?>
+
+
+</body>
+</html>
